bf9513ddfad3b248f90bf784d319fc86472188ef
OutsourcedGuru <support@outsourced.guru>
2018-09-14 13:08:42 -0700

    v1.0.6 Cleaned things up

--- tank-pi/sites/tracktest/routes/index.js
+++ tank-pi/sites/tracktest/routes/index.js
@@ -1,16 +1,9 @@
 var async =       require('async');
 var config =      require('./config');
 var express =     require('express');
-var fs =          require('fs');
-var Jimp =        require('jimp');
-var prototypes =  require('./prototypes');
-var request =     require('request');
 var wrappers =    require('./wrappers');
 var router =      express.Router();
 
-/*
-** Route /
-*/
 router.get('/', function(req, res, next) {
   async.waterfall([
     wrappers.initFindEdgesGraphic,
@@ -29,19 +22,3 @@ router.get('/', function(req, res, next) {
 }); // router.get()
 
 module.exports = router;
-
-/*
-** A description of the slope determination
-**
-** Imagine the following trend line as created from the samples taken at lines 1 (58% of height) and 2 (45% of height) of the image.
-** The vertical line would be the centerline as marked by the + in the After graphic. The top of the line represents the location of
-** the masking tape on the left at the second sample point; the bottom is the first. Subtracting the X component of each gives us
-** what's called the "run" or the adjacent side of a 90-degree triangle whose hypotenuse is that trend line. Subtracting the Y
-** components gives us the "rise". Dividing run/rise and adjusting radians to degrees gives us the angle.
-**
-** |    /
-** |   /
-** |  /
-** | /
-** .
-*/
--- tank-pi/sites/tracktest/routes/wrappers.js
+++ tank-pi/sites/tracktest/routes/wrappers.js
@@ -74,4 +74,18 @@ exports.writeOutput = function(img, callback) {
   });
 }
 
-
+/*
+** A description of the slope determination
+**
+** Imagine the following trend line as created from the samples taken at lines 1 (58% of height) and 2 (45% of height) of the image.
+** The vertical line would be the centerline as marked by the + in the After graphic. The top of the line represents the location of
+** the masking tape on the left at the second sample point; the bottom is the first. Subtracting the X component of each gives us
+** what's called the "run" or the adjacent side of a 90-degree triangle whose hypotenuse is that trend line. Subtracting the Y
+** components gives us the "rise". Dividing run/rise and adjusting radians to degrees gives us the angle.
+**
+** |    /
+** |   /
+** |  /
+** | /
+** .
+*/
